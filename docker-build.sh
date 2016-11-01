994b108da82b5a0c9649d078ce354f3d51d8ef71
Kim Neunert <kneunert@gmail.com>
2016-11-01 12:02:50 +0100

    added build-script and fixed entrypoint

---
+++ docker-build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build . -t k9ert/zcash-miner:latest
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,8 @@
 
 set -e
 
-ZCASH_HOME="/root/.zcash"
-ZCASH_CONF="${ZCASH_HOME}/zcash.conf"
-
-mkdir -p ${ZCASH_HOME}
-
-
 if [ "$1" = 'exec'  ]; then
-  exec gosu "$@"
+  "$@"
 else
   ./src/zcash-miner -stratum=stratum+tcp://zmine.io:1337 \
     -user=$1 -equihashsolver=tromp \
